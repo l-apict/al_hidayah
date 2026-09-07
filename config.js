65fbf61e64c22d449c6a507c72d60b090cf1d0b7
l-apict <khevaprasansa@gmail.com>
2026-09-07 09:00:41 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://khscktqunxezfeibzotr.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtoc2NrdHF1bnhlemZlaWJ6b3RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3MzQ3NjgsImV4cCI6MjEwNDMxMDc2OH0.C_O1yzywEhDcTpWCUcL1EtqDGJj2NiHwgwbYLZBKrX4';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
